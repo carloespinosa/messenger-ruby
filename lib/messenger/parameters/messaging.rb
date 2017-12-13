64859ec30a58e6722e80cb12cc624230f40ec3bc
Carlo Espinosa <498795+carloespinosa@users.noreply.github.com>
2017-12-13 17:10:01 +0900

    Added nlp to fb_params.first_entry

--- lib/messenger/parameters/messaging.rb
+++ lib/messenger/parameters/messaging.rb
@@ -1,11 +1,12 @@
 module Messenger
   module Parameters
     class Messaging
-      attr_accessor :sender_id, :recipient_id, :callback
+      attr_accessor :sender_id, :recipient_id, :nlp, :callback
 
       def initialize(sender:, recipient:, timestamp: nil, message: nil, delivery: nil, postback: nil, optin: nil, read: nil, account_linking: nil)
         @sender_id    = sender['id']
         @recipient_id = recipient['id']
+        @nlp          = message['nlp']
         @callback     = set_callback(message: message, delivery: delivery, postback: postback, optin: optin, read: read, account_linking: account_linking)
       end
 
